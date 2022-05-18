59ea54d18c1e5aa1d1ee00f2b6e5543d73054676
EC2 Default User <ec2-user@ip-172-31-70-254.ec2.internal>
2022-05-18 21:49:08 +0000

    Added basic variables

---
+++ Variables.py
@@ -0,0 +1,10 @@
+"""
+Variables
+"""
+a_str = "This is an example of a string in quotes" # string is abbreviated to str in Python
+my_float = 7.2
+an_integer = 5 # integer is abbreviated to int
+shopping_list = ["apples", "oranges", "pears"]
+a_dict = {"userId": "JBloggs"} # dictionary is abbreviated to dict
+example_tuple = ("red", "orange", "yellow")
+boolean_values = True # boolean is abbreviated to bool
