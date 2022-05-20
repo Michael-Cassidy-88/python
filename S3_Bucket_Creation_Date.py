4f8d979b75452b16ecd037709888c69cd09183ba
EC2 Default User <ec2-user@ip-172-31-70-254.ec2.internal>
2022-05-20 19:37:27 +0000

    Created code that retrieves bucket creation date

---
+++ S3_Bucket_Creation_Date.py
@@ -0,0 +1,9 @@
+import boto3
+
+s3 = boto3.client('s3')
+response = s3.list_buckets() # list buckets
+
+buckets = response["Buckets"] # set the variable "buckets" to respond with the list of buckets requested
+
+for bucket in buckets: 
+    print(bucket["CreationDate"]) # prints the creation date of each bucket within the variable "buckets".
