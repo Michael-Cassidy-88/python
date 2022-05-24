5907aa9be90d230211673d991a35ef17d3d42818
EC2 Default User <ec2-user@ip-172-31-68-255.ec2.internal>
2022-05-24 01:08:44 +0000

    Connect DynamoDB to Cloud9

--- Dynamo_DB/connect_to_dynamodb_with_boto3.py
+++ Dynamo_DB/connect_to_dynamodb_with_boto3.py
@@ -4,6 +4,6 @@ import boto3
 
 dynamodb = boto3.resource(
     'dynamodb',
-    aws_access_key_id='AKIASXQ7GL7FZ6PDYVXS',
-    aws_secret_access_key='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',
+    aws_access_key_id='*****',
+    aws_secret_access_key='*****',
     )
